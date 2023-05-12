ee2f77a91ce7778635f85810293464cd773940bd
BigBlueTree <124953905+BigBlueTree@users.noreply.github.com>
2023-05-12 18:32:51 +0900

    Add files via upload

---
+++ 0512/15Inheritance.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname=fname
+        self.lastname=lname
+    def printname(self):
+        print(self.firstname,self.lastname)
+        
+#x=Person('솔','라')
+#x.printname()
+class Member(Person):
+    def __init__(self, fname, lname, grade):
+        super().__init__(fname,lname,)
+        self.grade=grade
+        
+    def welcome(self):
+        print("Welcome",self.firstname,self.lastname,self.grade)
+        
+    def printname(self):
+        print(self.firstname,self.lastname,self.grade)
+
+x=Member('Sola','Sun','Hero')
+x.printname()
