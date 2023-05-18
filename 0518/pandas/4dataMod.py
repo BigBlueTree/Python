151894279d38a73b8205644fd51f8b56361f80ce
BigBlueTree <124953905+BigBlueTree@users.noreply.github.com>
2023-05-18 14:48:15 +0900

    Add files via upload

--- 0518/pandas/4dataMod.py
+++ 0518/pandas/4dataMod.py
@@ -4,12 +4,17 @@ import seaborn as sns
 
 s = pd.Series(range(10))
 s[3] = np.nan
-print(s,"\n",s.count())
+print(s,"\n",s.count(),"\n",s.size)
 np.random.seed(2)
 df = pd.DataFrame(np.random.randint(5, size=(4, 4)), dtype=float)
 df.iloc[2, 3] = np.nan
-print(df,"\n",df.count())
+print(df,"\n",df.count(),"\n",df.size)
 
 titanic = sns.load_dataset("titanic")
 print(titanic)
 print(titanic.head())
+print(titanic.size,'\n',titanic.count())
+print(titanic['deck'].count())
+print(titanic['deck'].size)
+print(titanic['deck'].unique())
+print(titanic['deck'].value_counts())
